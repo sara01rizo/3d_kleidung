61d61f209815132ec4ef160957509cb3ec3c11a6
sara pulido <sara01rizo@gmail.com>
2023-01-23 12:15:24 +0100

    Create: initial components time to switch for TypeScript prob

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
-  );
+import { ThemeProvider } from 'styled-components'
+import { Container } from './components/styles/Container.styled'
+import Banner from './components/Banner.js'
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+  },
+  mobile: '768px',
 }
 
-export default App;
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <Banner />
+        <Container>
+        </Container>
+      </>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import { StyledBanner } from './styles/Banner.styled.js'
+import { Container } from './styles/Container.styled'
+
+export default function Banner() {
+  return (
+    <StyledBanner>
+        <Container>
+
+        </Container>
+    </StyledBanner>
+  )
+}
+
